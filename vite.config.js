ae0678c3143b5980dd4d1dfb261933b0ce2de432
kiruba-33 <kirubakaran1890@gmail.com>
2026-05-09 21:19:21 +0530

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
 
   build: {
+    chunkSizeWarningLimit: 1000,
+
     rollupOptions: {
       output: {
-        manualChunks: {
-          react: ['react', 'react-dom'],
-          router: ['react-router-dom'],
-          motion: ['framer-motion'],
-          gsap: ['gsap'],
-          icons: ['lucide-react'],
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) {
+              return 'react'
+            }
+
+            if (id.includes('react-router-dom')) {
+              return 'router'
+            }
+
+            if (id.includes('framer-motion')) {
+              return 'motion'
+            }
+
+            if (id.includes('gsap')) {
+              return 'gsap'
+            }
+
+            if (id.includes('lucide-react')) {
+              return 'icons'
+            }
+          }
         },
       },
     },
