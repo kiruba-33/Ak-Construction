5402c493b5803c1802e0c663acca63b86938f8f9
kiruba-33 <kirubakaran1890@gmail.com>
2026-05-09 21:13:03 +0530

    spilt a js bundles

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          router: ['react-router-dom'],
+          motion: ['framer-motion'],
+          gsap: ['gsap'],
+          icons: ['lucide-react'],
+        },
+      },
+    },
+  },
 })
